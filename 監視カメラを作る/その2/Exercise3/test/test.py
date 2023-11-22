916629295263801ee1e4f81b47aabf89b7726d56
Shiraishi-Shodai <siranosuke1227@gmail.com>
2023-11-22 13:03:48 +0900

    2023/11/22 opencvで写真撮影の途中

--- 監視カメラを作る/その2/Exercise3/test/test.py
+++ 監視カメラを作る/その2/Exercise3/test/test.py
@@ -1,31 +1,54 @@
 from flask import render_template, Flask, Response
 import cv2 as cv
 
-
 app = Flask(__name__, template_folder="../templates")
 
-
-camera = cv.VideoCapture(0)
-
 def gen_frames():
+    
    while True:
+       camera = cv.VideoCapture(0)
        success, frame = camera.read()
        if not success:
            break
        else:
-           #フレームデータをjpgに圧縮
+           #フレームデータをjpgに圧縮(ret: bool, buffer: ndarray)
            ret, buffer = cv.imencode('.jpg',frame)
            # bytesデータ化
            frame = buffer.tobytes()
+           # カメラを解放
+           camera.release()
+            # yield 莫大な量の戻り値を小分けにして返すことが出来る
+            # 新しいフレーム（画像）をブラウザに送信
            yield (b'--frame\r\n'
                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        
 
 @app.route('/video_feed')
 def video_feed():
+    # HTTPレスポンス
    #imgタグに埋め込まれるResponseオブジェクトを返す
-   return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # デフォルトでは、ResponseオブジェクトはHTMLのmimetypeを持ちます。つまり、デフォルトのContent-Typeヘッダーは"text/html"に設定されています。
+    # mimetype='multipart/x-mixed-replace; boundary=frame'は、HTTPレスポンスのContent-Typeヘッダーを設定します。
+    # このヘッダーは、レスポンスの形式をブラウザに伝えます。multipart/x-mixed-replaceは、マルチパートレスポンスの各部分が前の部分を置き換えることを示します。これにより、ブラウザは新しい画像フレームを受信するたびに、前のフレームを新しいフレームで置き換え、リアルタイムのビデオストリームを表示します。
 
+    # boundary: HTTPレスポンスの情報を1つずつ区別するために用いる
+    '''
+    例)
+    
+    Content-Type: multipart/form-data; boundary=aBoundaryString
+    (マルチパート文書全体に関連付けられる、他のヘッダー)
+
+    (データ)
+    --aBoundaryString
+    Content-Disposition: form-data; name="myField"
+
+    (データ)
+    --aBoundaryString
+    (サブパート)
+    --aBoundaryString--
+    '''
+
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 @app.route('/index')
